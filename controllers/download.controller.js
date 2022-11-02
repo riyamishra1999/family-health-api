a2d53781598150776f1032a5d9115fd68a9de60e
manojpudasaini <mpudasaini17@gmail.com>
2022-11-02 22:26:14 +0545

    added multiple reports

--- controllers/download.controller.js
+++ controllers/download.controller.js
@@ -9,6 +9,9 @@ const path = require("path");
 hbs.registerHelper("counter", function (num) {
   return num + 1;
 });
+hbs.registerHelper("getDate", function () {
+  return new Date().toISOString().split("T")[0];
+});
 exports.createForm = async (req, res) => {
   const id = req.params.id;
   const compile = async function (templateName, data) {
@@ -47,6 +50,45 @@ exports.createForm = async (req, res) => {
       await browser.close();
       // return res.send("success");
       return res.sendFile(`${__basedir}/${id}-report.pdf`);
+    } catch (e) {
+      console.log(e);
+    }
+  })();
+};
+
+exports.createMultipleForm = async (req, res) => {
+  const id = req.params.id;
+  const compile = async function (templateName, data) {
+    const filePath = path.join(
+      process.cwd(),
+      "templates",
+      `${templateName}.hbs`
+    );
+    const html = await fs.readFile(filePath, "utf8");
+    console.log(html);
+    return hbs.compile(html)(data);
+  };
+  (async function () {
+    try {
+      const browser = await puppeteer.launch();
+
+      const page = await browser.newPage();
+      const data = req.body;
+      console.log(data, "data from the client");
+
+      const content = await compile("multiple", data);
+
+      console.log(content);
+      await page.setContent(content);
+      await page.emulateMediaType("screen");
+      await page.pdf({
+        path: `${id}-multiple-report.pdf`,
+        format: "letter",
+        printBackground: true,
+      });
+      console.log("done creating pdf");
+      await browser.close();
+      return res.sendFile(`${__basedir}/${id}-multiple-report.pdf`);
     } catch (e) {
       console.log(e);
     }
--- routes/download.route.js
+++ routes/download.route.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const formController = require("../controllers/download.controller");
 
 router.post("/create-form/:id", formController.createForm);
+router.post("/create-multiple-form/:id", formController.createMultipleForm);
 router.get("/download-form/:id", formController.sendPdf);
 
 module.exports = router;
